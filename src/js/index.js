1438494dc45511e9988ab95d78f4e01cf1922712
Archangel2903 <archangel2903@gmail.com>
2020-09-21 04:03:11 +0300

    update

--- src/js/index.js
+++ src/js/index.js
@@ -6,6 +6,8 @@ import 'jquery-ui/ui/effect'
 import 'bootstrap';
 import 'popper.js';
 import Swiper from 'swiper/dist/js/swiper.min';
+import noUiSlider from 'nouislider';
+
 
 $(window).on('load', function () {
     let b = $('body');
@@ -20,6 +22,85 @@ $(window).on('load', function () {
 });
 
 $(function () {
+    // Swiper slider
+    if ($('.swiper-container').length) {
+        let slider;
+        let slide = document.querySelectorAll('.swiper-container .swiper-slide').length;
+
+        if (slide > 1) {
+            slider = new Swiper('.swiper-container', {
+                observer: true,
+                observeParents: true,
+                loop: true,
+                autoplay: true,
+                spaceBetween: 25,
+                slidesPerView: 1,
+                navigation: {
+                    nextEl: '.swiper-button-next',
+                    prevEl: '.swiper-button-prev'
+                },
+                pagination: {
+                    el: '.swiper-pagination',
+                    clickable: true
+                },
+                /*scrollbar: {
+                    el: '.swiper-scrollbar',
+                },*/
+                dynamicBullets: true,
+            });
+        }
+    }
+
+    // Range slide
+    if ($('input[type="range"]')) {
+        let sliderRange = document.querySelectorAll('.slider-range');
+        let sliderHandles = document.querySelectorAll('.slider-handles');
+
+        if (sliderRange.length) {
+            sliderRange.forEach(function (elem) {
+                let input = elem.childNodes[0];
+                let startValue = input.hasAttribute('value') ? Number(input.getAttribute('value')) : 1;
+                let minValue = input.hasAttribute('min') ? Number(input.getAttribute('min')) : 1;
+                let maxValue = input.hasAttribute('max') ? Number(input.getAttribute('max')) : 100;
+
+                input.remove();
+
+                noUiSlider.create(elem, {
+                    start: [startValue],
+                    step: 1,
+                    behavior: 'tap',
+                    connect: [true, false],
+                    range: {
+                        'min': [minValue],
+                        'max': [maxValue]
+                    }
+                });
+            });
+        }
+
+        if (sliderHandles.length) {
+            sliderHandles.forEach(function (elem) {
+                let input = elem.childNodes[0];
+                let minValue = input.hasAttribute('min') ? Number(input.getAttribute('min')) : 1;
+                let maxValue = input.hasAttribute('max') ? Number(input.getAttribute('max')) : 100;
+
+                input.remove();
+
+                noUiSlider.create(elem, {
+                    start: [minValue, maxValue/2],
+                    step: 1,
+                    behavior: 'tap-drag',
+                    connect: true,
+                    range: {
+                        'min': minValue,
+                        'max': maxValue
+                    }
+                });
+            });
+        }
+    }
+
+    // Lazy load observer
     const imagesAll = document.querySelectorAll('img[data-src]');
     let imgObserve = new IntersectionObserver(function (entries) {
         entries.forEach(function (entry) {
@@ -36,33 +117,5 @@ $(function () {
         imagesAll.forEach(function (image) {
             imgObserve.observe(image);
         });
-    }
-
-    if ($('.swiper-container').length) {
-        let slider;
-        let slide = document.querySelectorAll('.swiper-container .swiper-slide').length;
-
-        if (slide > 2) {
-            slider = new Swiper('.swiper-container', {
-                observer: true,
-                observeParents: true,
-                loop: true,
-                autoplay: true,
-                spaceBetween: 25,
-                slidesPerView: 2,
-                navigation: {
-                    nextEl: '.swiper-button-next',
-                    prevEl: '.swiper-button-prev'
-                },
-                pagination: {
-                    el: '.swiper-pagination',
-                    clickable: true
-                },
-                scrollbar: {
-                    el: '.swiper-scrollbar',
-                },
-                dynamicBullets: true,
-            });
-        }
     }
 });
